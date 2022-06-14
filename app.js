1be7edd420e85a7d150724f15433cba357964463
Davipbr15 <davipbr90@gmail.com>
2022-06-14 15:02:45 -0300

    Pos body parser

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require("express");
-
 const app = express();
 
-app.get('/', (req,res)=>{
+const bodyParser = require('body-parser');
 
-    res.status(300).send("Products");
+//middleWare
 
-    
+app.use(bodyParser.json());
 
+app.get("/", (req,res)=>{
+    res.send("Sample Page <br>Go to /products");
+})
+
+app.get('/products', (req,res)=>{
+
+    const product = {
+        id:1,
+        name:"Monitor Hazer",
+        price:159.35,
+        image:"url_image"
+    }
+
+    res.send(product);
+
+})
+
+app.post("/products", (req,res)=>{
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
 })
 
 app.listen(3001, () => {
